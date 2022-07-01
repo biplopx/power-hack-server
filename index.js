3816c53c36d7a34c44aa772602e502c3726929f9
MD Biplop Hossain <developerbiplop@gmail.com>
2022-07-01 18:56:10 +0600

    fix page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var ObjectId = require('mongodb').ObjectID;
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
@@ -44,10 +45,10 @@ async function run() {
     console.log('DB Connect');
     const usersCollection = client.db('powerhack').collection('users');
     const billingCollection = client.db('powerhack').collection('billings');
+    billingCollection.createIndex({ fullName: "text", email: "text", phone: "text" })
 
     // user creation
     app.post('/api/registration', async (req, res) => {
-      console.log(req.body)
       const email = req.body.email;
       const fullName = req.body.fullName;
       const password = req.body.password;
@@ -74,7 +75,6 @@ async function run() {
       const email = req.body.email;
       const password = req.body.password;
       const requesterAccount = await usersCollection.findOne({ email: email });
-      console.log(process.env.PH_ACCESS_TOKEN)
       if (requesterAccount) {
         const isPasswordCorrect = bcryptjs.compareSync(password, requesterAccount.hashedPassword);
         if (isPasswordCorrect) {
@@ -100,15 +100,43 @@ async function run() {
         fullName,
         email,
         phone,
-        paidAmount
+        paidAmount,
+        created_at: Date.now()
       }
       const result = await billingCollection.insertOne(billing);
       res.send({ status: 200, message: `Add successful from ${req.decoded}` })
     });
 
     // Billings List
-    app.get('/api/billing-list', async (req, res) => {
+    app.get('/api/billing-list', verifyJWT, async (req, res) => {
+      const billings = await billingCollection.find().sort({ created_at: -1 }).toArray();
+      const page = parseInt(req.query.page) || 1;
+      // get pager object for specified page
+      const pageSize = 10;
+      const pager = paginate(billings.length, page, pageSize);
+      // get page of items from items array
+      const pageOfItems = billings.slice(pager.startIndex, pager.endIndex + 1);
+      res.send({ pager, pageOfItems });
+
+    });
+
+    // Billings List TotalPaidAmount
+    app.get('/api/billing-total', verifyJWT, async (req, res) => {
       const billings = await billingCollection.find().toArray();
+      let sum = 0;
+      billings.forEach(
+        pageItem => {
+          sum += parseInt(pageItem.paidAmount)
+        }
+      )
+      res.send({ status: 200, total: sum });
+
+    });
+
+    // Billings List Search
+    app.get('/api/billing-search', verifyJWT, async (req, res) => {
+      const searchTerm = req.query.search
+      const billings = await billingCollection.find({ $text: { $search: searchTerm } }).sort({ created_at: -1 }).toArray();
       const page = parseInt(req.query.page) || 1;
       // get pager object for specified page
       const pageSize = 10;
@@ -120,13 +148,20 @@ async function run() {
     });
 
     // Edit Billing
-    app.patch('/api/update-billing/:id', async (req, res) => {
+    app.put('/api/update-billing/:id', verifyJWT, async (req, res) => {
       const id = req.params.id;
       const newBilling = req.body;
-      console.log(newBilling)
-      const filter = { _id: ObjectId(id) };
-      const updatedBilling = await billingCollection.updateOne(filter, newBilling);
+      const updatedBilling = await billingCollection.updateOne({ _id: ObjectId(id) }, newBilling);
       res.send(updatedBilling);
+    })
+
+    // Edit Billing
+    app.delete('/api/delete-billing/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const billings = await billingCollection.deleteOne({ _id: ObjectId(id) })
+      console.log(billings)
+      res.send({ status: 200, message: 'Deleted Succesfully' });
     })
 
   }
